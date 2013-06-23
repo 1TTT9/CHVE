6856d666850543a03dca0088babb3a066492805b
ggc <ggc@ggMBA.local>
2013-06-23 21:20:02 +0900

    add cln n serv codes

---
+++ pongclient_v6.py
@@ -0,0 +1,206 @@
+import math, random, os, sys, time
+from ctypes import *
+import pygame
+
+from pygame import sprite
+from pygame.locals import *
+
+"""
+Tmeplate:
+  This file is used as a template for pygame programing.
+  Do not pollute this file with appending other codes.
+"""
+(width, height) =(720,720)
+background_colour = (255,255,255)
+ball_colour = (255,0,0)
+elasticity = 0.80
+pygame.init()
+pygame.display.set_caption('Template')
+#pygame.mouse.set_visible(False)
+screen = pygame.display.set_mode((width, height))
+
+# used for spritegroup (both players and ball)
+vpgroup = []
+
+
+"""
+[function name, usage, arguments]
+#screen.blit( source, dest, area=None );
+  usage:  
+    draw one image/(source surface) onto another
+  argument:
+    source - source surface
+    dest - dest can be either pairs of coordinates representing the upper left corner of the source or dest.
+    area - a small portion of source surface to draw
+#surface.convert()
+  usage:  
+    change the pixel format of an image with corresponding to the monitor. It help fasten the speed whenever SDL requires to do blit-action.
+#surface.convert_alpha()
+  usage:
+    change the pixel format of an image including pixel alpha. Basically speaking, it enable transparency of an image.
+"""	
+
+def drawDemo(bg):
+	font = pygame.font.Font(None, 36)
+	text = font.render("This is a template.", 1, (10,10,10))
+	textpos = text.get_rect()	
+
+	textpos.center = bg.get_rect().center
+	bg.blit( text, textpos )
+	( x, y ) = ( textpos.x, textpos.y )
+	for i in range(1,5):
+		textpos = ( x, y-20*i )
+		bg.blit( text, textpos )
+		textpos = ( x, y+20*i )
+		bg.blit( text, textpos )
+
+def isRunning():
+	is_running = True
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			is_running = False
+		elif event.type == pygame.KEYDOWN:	
+			if event.key == pygame.K_ESCAPE:
+				is_running = False
+			elif event.key == pygame.K_DOWN:
+				if master is not None:
+					master.reset()
+			elif event.key == pygame.K_UP:
+				pass		
+	return is_running
+
+
+def load_png( fname, radius = 50 ):
+	if fname.endswith('png'):
+		w,h = [ (width, height) if fname.startswith('bg') else (102,77) ][0]
+		try:
+			image = pygame.image.load(fname)
+			image = pygame.transform.scale( image, (w, h) ) 
+			image = [ image.convert() if image.get_alpha() is None else image.convert_alpha() ][0]
+		except pygame.error, msg:
+			print 'Cannot load image', bg_fname
+			raise SystemExist, msg
+		return image, image.get_rect()
+	else:
+		color =  [ (50,50,250,200) if not fname.startswith("ball") else ball_colour ][0]
+		circle = pygame.Surface( [radius*2]*2, SRCALPHA, 32  )
+		circle = circle.convert_alpha()
+		circle_rect = pygame.draw.circle( circle, color, [radius]*2, radius )
+		return circle, circle_rect	
+
+class VPlayer(sprite.Sprite):
+	def __init__( self, name, xy, radius = 50 ):
+		sprite.Sprite.__init__( self )
+		self.origin = xy
+		self.radius = radius
+		self.image, self.rect = load_png( name, radius )
+		self.area = screen.get_rect()
+                self.name = name
+		self.rect.topleft = map(int, xy)
+                self.movpos = [0]*2
+                self.pos = [0]*2
+
+        def update( self ):
+                self.rect.topleft = ( 0,0 )
+                self.rect = self.rect.move( self.pos )
+
+        def move( self, p ):
+                self.pos = p
+	
+
+def main():
+	### fill background
+	bg = pygame.Surface( screen.get_size() )
+	bg = bg.convert()
+	bg.fill( background_colour )
+
+	#bgimg, bgrect = load_png('bg_dark.png')
+	bgimg, bgrect = bg, bg.get_rect()
+	bgwimg, bgwrect = load_png('bg_wire.png')
+
+	
+	### auxiliary line
+	nblock = width/30
+	for i in range(1,nblock):
+		pygame.draw.line( bgimg, (55, 55, 55), (0, height*i/nblock), (width, height*i/nblock), 2 )
+		pygame.draw.line( bgimg, (55, 55, 55), (width*i/nblock, 0), (width*i/nblock, height), 2 )
+
+
+	### border
+	global borders
+	thickness = height*0.05
+	BDL = pygame.draw.rect( bgimg, (35,0,0), pygame.Rect( 0, 0, thickness, height ) )
+	BDR = pygame.draw.rect( bgimg, (35,0,0), pygame.Rect( width-thickness,0,thickness, height ) )
+	BDT = pygame.draw.rect( bgimg, (35,0,0), pygame.Rect( 0, 0, width, thickness ) )
+	BDB = pygame.draw.rect( bgimg, (35,0,0), pygame.Rect( 0, height-thickness, width, thickness ) )
+	borders = [BDL, BDR, BDT, BDB]
+
+
+	### demo 
+	#drawDemo(bg)
+
+
+        global vpgroup
+	### ball
+        ball = VPlayer('ball', ( width*0.3,height*0.3 ), 50 )
+        vpgroup.append(ball)
+
+	### player
+	player = VPlayer( "player", ( width*0.5,height*0.5 ), 25 )
+	vpgroup.append(player)
+        
+
+        vpsprite = []
+        for vp in vpgroup:
+                vpsprite.append( sprite.RenderPlain(vp) )
+
+	### draw background-color
+	#screen.blit( bg, (0,0) )
+	screen.blit( bgimg, bgrect )
+	screen.blit( bgwimg, bgwrect )
+
+	### flip
+	pygame.display.flip()
+
+
+	clock = pygame.time.Clock()
+	t_start = time.time()
+	while (1):
+
+                t_bc = time.time()
+		### detect mouse/keyboard event
+		if not isRunning():
+			break
+
+		clock.tick(30)
+
+                for vp in vpgroup:
+                        vp.move( vp.origin )
+
+                time.sleep(1./40)
+
+		t_measure = [ 1/30. if clock.get_fps() < 30 else 1./clock.get_fps() ][0]
+
+		### blit
+                for vp in vpgroup:
+                        screen.blit(  bgimg, vp.rect, vp.rect )
+                        screen.blit( bgwimg, vp.rect, vp.rect )
+
+                ### update
+                for vps in vpsprite:
+                        vps.update()
+
+		### draw
+                for vps in vpsprite:
+                        vps.draw( screen )
+		
+		pygame.display.set_caption( 'fps: {0:4.2f}'.format(clock.get_fps()) )
+
+		pygame.display.flip()
+
+                t_ec = time.time()
+#                print 'execution interval: {0:5.2f}, ({1:5.2f}, {2:5.2f})'.format( 1/(t_ec-t_bc), t_bc, t_ec )
+
+
+if __name__  == '__main__':
+	main()
